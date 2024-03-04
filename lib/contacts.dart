4a8b77ba2ba69662d51e2ab5b6d1ce7df712817f
aymenzouaoui <118661290+aymenzouaoui@users.noreply.github.com>
2024-03-05 00:04:20 +0100

    Update contacts.dart

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -1,16 +1,260 @@
+import 'package:chat/screens/messages/messages_screen.dart';
 import 'package:flutter/material.dart';
 
-class ContactsPage extends StatelessWidget {
-  const ContactsPage({Key? key}) : super(key: key);
+class Avatar extends StatelessWidget {
+  final double size;
+  final String image;
+  final EdgeInsets margin;
+  final bool hasBorder;
+  final double elevation;
+
+  const Avatar({
+    Key? key,
+    required this.size,
+    required this.image,
+    this.margin = EdgeInsets.zero,
+    this.hasBorder = false,
+    this.elevation = 0.0,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Contacts'),
+    return Padding(
+      padding: margin,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: AssetImage(image),
+            fit: BoxFit.cover,
+          ),
+          border: hasBorder
+              ? Border.all(
+                  color: Colors.white,
+                  width: 2.0,
+                )
+              : null,
+          boxShadow: hasBorder
+              ? [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: elevation,
+                    spreadRadius: elevation / 2,
+                  ),
+                ]
+              : null,
+        ),
       ),
-      body: Center(
-        child: Text('This is the Contacts Page'),
+    );
+  }
+}
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatsScreenState createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1D194E),
+      body: SafeArea(
+        child: Column(
+          children: [
+            _top(),
+            _body(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _top() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(30, 30, 30, 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Cross Chat with your friends',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(14),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.black12,
+                ),
+                child: Icon(
+                  Icons.search,
+                  size: 30,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(width: 15),
+              Expanded(
+                child: Container(
+                  height: 100,
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 8,
+                    itemBuilder: (context, index) {
+                      return Avatar(
+                        size: 60,
+                        margin: EdgeInsets.only(right: 15),
+                        image: 'assets/image/${index + 1}.jpg',
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Divider(
+            color: Colors.white,
+            thickness: 1,
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _body() {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(45),
+            topRight: Radius.circular(45),
+          ),
+          color: Colors.white,
+        ),
+        child: ListView(
+          padding: EdgeInsets.only(top: 35),
+          physics: BouncingScrollPhysics(),
+          children: [
+            _itemChats(
+              avatar: 'assets/image/2.jpg',
+              name: 'Johnny Doe',
+              chat:
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+              time: '08.10',
+            ),
+            Divider(color: Colors.grey, thickness: 0.5),
+            _itemChats(
+              avatar: 'assets/image/4.jpg',
+              name: 'Adrian',
+              chat: 'Excepteur sint occaecat cupidatat non proident',
+              time: '03.19',
+            ),
+            Divider(color: Colors.grey, thickness: 0.5),
+            _itemChats(
+              avatar: 'assets/image/5.jpg',
+              name: 'Fiona',
+              chat: 'Hii... 😎',
+              time: '02.53',
+            ),
+            Divider(color: Colors.grey, thickness: 0.5),
+            _itemChats(
+              avatar: 'assets/image/6.jpg',
+              name: 'Emma',
+              chat: 'Consectetur adipiscing elit',
+              time: '11.39',
+            ),
+            Divider(color: Colors.grey, thickness: 0.5),
+            _itemChats(
+              avatar: 'assets/image/7.jpg',
+              name: 'Alexander',
+              chat:
+                  'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+              time: '00.09',
+            ),
+            Divider(color: Colors.grey, thickness: 0.5),
+            _itemChats(
+              avatar: 'assets/image/8.jpg',
+              name: 'Alsoher',
+              chat:
+                  'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+              time: '00.09',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _itemChats(
+      {String avatar = '', name = '', chat = '', time = '00.00'}) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => MessagesScreen(),
+          ),
+        );
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 20),
+        elevation: 0,
+        child: Row(
+          children: [
+            Avatar(
+              margin: EdgeInsets.only(right: 20),
+              size: 60,
+              image: avatar,
+              hasBorder: true, // Add border to avatar
+              elevation: 2, // Add shadow effect
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '$name',
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '$time',
+                        style: TextStyle(
+                            color: Colors.grey, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '$chat',
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
